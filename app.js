03b142b85a38b34e04c0663adf5f02c2f8711c25
Lloyd Cledwyn <cledwyn@gmail.com>
2015-05-04 20:50:48 -0500

    wire up fb js

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-var element = document.querySelector("#greeting");
-element.innerText = "Hello, world!";
+var username = document.getElementById('username');
+var message = document.getElementById('message');
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var login = document.getElementById('login');
+
+
+var baseRef = new Firebase("https://YOURURL.firebaseIO.com");
+var messagesRef = baseRef.child("messages");
+
+
+// Retrieve new posts as they are added to Firebase
+messagesRef.on("child_added", function(snapshot) {
+  var newPost = snapshot.val();
+  
+  var msg = document.createElement("div");
+  msg.innerText = newPost.username + ": " + newPost.message;
+  
+  messages.appendChild(msg);
+  console.log(newPost);
+  
+});
