ea28166599f1c80b8b3d3d486f7319b9581b5381
Lloyd Cledwyn <cledwyn@gmail.com>
2015-09-12 11:26:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+// define page elements
 var username = document.getElementById('username');
 var message = document.getElementById('message');
 var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var login = document.getElementById('login');
 
-
+// define Firebase ref
 var baseRef = new Firebase("https://YOURURL.firebaseIO.com");
 var messagesRef = baseRef.child("messages");
-
 
 // Retrieve new posts as they are added to Firebase
 messagesRef.limitToLast(20).on("child_added", function(snapshot) {
