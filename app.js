5517bf79601e949d04d23ef9338f0b4df653a935
Lloyd Cledwyn <cledwyn@gmail.com>
2016-09-16 08:20:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var element = document.querySelector("#greeting");
-element.innerText = "Hello, world!";
+    // Initialize Firebase
+    // TODO: Replace with your project's customized code snippet
+    var config = {
+      apiKey: "<API_KEY>",
+      authDomain: "<PROJECT_ID>.firebaseapp.com",
+      databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+      storageBucket: "<BUCKET>.appspot.com",
+    };
+    firebase.initializeApp(config);
